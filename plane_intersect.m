ea366b73e492aaf50cf2b8a4a0f3c28ef83bef88
germangb <germangb42@gmail.com>
2016-11-28 18:35:34 +0100

    plane intersect

--- plane_intersect.m
+++ plane_intersect.m
@@ -8,8 +8,9 @@ function [l, normal] = plane_intersect( ray, plane )
     % l = (-D - px*A - py*B - pz*C) / (rx*A + ry*B + rz*C)
     
     % compute intersection
-    l = (-plane(4) - plane(1)*ray(1) - plane(2)*ray(2) - plane(3)*ray(3)) / ...
-        (plane(1)*ray(4) + plane(2)*ray(5) + plane(3)*ray(6));
+    %l = (-plane(4) - plane(1)*ray(1) - plane(2)*ray(2) - plane(3)*ray(3)) / ...
+    %    (plane(1)*ray(4) + plane(2)*ray(5) + plane(3)*ray(6));
+    l = (-plane(4) - plane(1:3)*ray(1:3)') / (plane(1:3)*ray(4:6)');
     
     if l < 0
         l = inf;
